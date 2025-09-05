662104145affff42d69b0ed3f94e48780831acfc
brunomrtns <brunomartinsss@gmail.com>
2025-09-04 22:45:50 -0300

    update

--- fix-vm-network.sh
+++ fix-vm-network.sh
@@ -2,9 +2,22 @@
 
 set -e
 
+# Checa se está sendo executado como root
 [ "$(id -u)" -ne 0 ] && echo "Este script deve ser executado como root." && exit 1
 
+# Detecta interface ativa que não seja loopback nem a bridge do Incus
+OUT_IF=$(ip -br a | awk '$2=="UP" && $1!="lo" && $1!="incusbr0" {print $1; exit}')
+
+if [ -z "$OUT_IF" ]; then
+    echo "Não foi possível detectar a interface de rede ativa."
+    exit 1
+fi
+
+echo "Interface de saída detectada: $OUT_IF"
+
+# Regras de iptables
 iptables -L FORWARD -v -n
 iptables -P FORWARD ACCEPT
-iptables -A FORWARD -i incusbr0 -o enp2s0 -j ACCEPT
-iptables -A FORWARD -o incusbr0 -i enp2s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+iptables -A FORWARD -i incusbr0 -o "$OUT_IF" -j ACCEPT
+iptables -A FORWARD -o incusbr0 -i "$OUT_IF" -m state --state RELATED,ESTABLISHED -j ACCEPT
